fd67ee2517ae3e00ca2ac4779e795f0475c87383
Your Name <you@example.com>
2024-10-21 14:54:15 +0300

    Added comments, edit & update function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,36 +7,55 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    // Index Function
     public function index() {
-
         $posts = Post::all();
         return view('posts.index', ['allPosts' => $posts]);
         //return "<h1>All posts</h1>";
     }
 
+    // Show Function
     public function show($id) {
-
        // \Log::debug($id);
        $post = Post::find($id);
        return view('posts.show', ['post' => $post]);
     }
 
+    // Create Function
     public function create() {
-
         $post = Post::all();
         return view('posts.create');
     }
 
+    // Store Function
     public function store(Request $request) {
-
         // \Log::debug($request);
         $data = [
             'title' => $request->title,
             'content' => $request->content
         ];
 
-        $post = Post::create($data);
-
+        Post::create($data);
         return redirect('/posts');;
+    }
+
+    // Edit Function
+    public function edit($id) {
+        $post = Post::find($id);
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    // Update Function
+    public function update(Request $request, $id) {
+        $post = Post::find($id);
+
+        $data = [
+            'title' => $request->title,
+            'content' => $request->content
+        ];
+
+        $post->update($data);
+
+        return redirect('/posts');
     }
 }
